8a146814ebd09278e517249b28b9fe6d34371352
Muna S <munasaber@gmail.com>
2026-07-28 18:13:58 -0700

    Added gate application functions

--- mathematical_simulations/quantum_gate_solver.py
+++ mathematical_simulations/quantum_gate_solver.py
@@ -3,16 +3,22 @@
 
 
 def apply_gate(qubit: np.array, gate_type: str):
-    """This function applies a Hadamard or NOT (Pauli-X) gate to a qubit
+    """
+    This function applies a Hadamard or NOT (Pauli-X) gate to a qubit
 
     Inputs
     ------
     qubit: np.array
         Qubit that is the object on which gate operations will be performed
     gate_type: str
-        Gate to be applied to the qubit with options "Hadarmard or NOT"
+        Gate to be applied to the qubit with options "Hadamard or NOT"
 
     """
-
-
+    if gate_type.lower()=="hadamard":
+        gate=1/np.sqrt(2)*np.array([[1 , 1],[1 , -1]])
+    elif gate_type.lower()=="not":
+        gate=np.array([[0, 1],[1 , 0]])
+    else:
+        raise ValueError("Incorrect gate_type. Please enter Hadamard or NOT for the gate_type")
+    return gate@qubit
 

e5f1a3559e882af47c9a57561ff5613371223891
Muna S <munasaber@gmail.com>
2026-08-10 22:01:57 -0700

    Updated Shor's algorithm documenation

--- quantum_algorithms/Shor.py
+++ quantum_algorithms/Shor.py
@@ -4,22 +4,34 @@ import math
 import random
 
 
-def is_prime(number: int):
+def is_prime(N: int):
     """
-    Tests if a number is prime.
+    Tests if a number, n, is prime.
+    
+    Inputs
+    ------
+    N : int
+        A composite integer that is the product of two or more distinct prime numbers. 
     """
-    if n%2==0 or n==1:
+    if N%2==0 or N==1:
         return False
-    elif n==2:
+    elif N==2:
         return True
-    for d in range(3, int(math.sqrt(n))+1, 2):
-        if n%d==0:
+    for d in range(3, int(math.sqrt(N))+1, 2):
+        if N%d==0:
             return False
     return True
 
 def find_lowest_periond(a, N):
     """
     Find the lowest period, r.
+    
+    Inputs
+    ------
+    N : int
+        A composite integer that is the product of two or more distinct prime numbers. 
+    a : int
+        An integer that is coprime to N and chosen at random. 
     """
     for r in range(1, N, 1):
         remainder=(a^r)%N
@@ -63,5 +75,7 @@ def Shors_algorithm(N: int):
     if c%N ==1 or c%N ==-1:
         a=random.randint(1, N)
         r=find_period(a, N)
+
+
 
     

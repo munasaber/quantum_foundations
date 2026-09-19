1ad1a930424c29c3ba9ecb3ef46a790fbb5a5315
Muna S <munasaber@gmail.com>
2026-09-18 20:23:23 -0700

    Constructed the initial state of the circuit and mapping

--- molecular_orbitals/Hydrogen.py
+++ molecular_orbitals/Hydrogen.py
@@ -1,10 +1,26 @@
 import qiskit_nature
 import qiskit
 from qiskit_nature.second_q.drivers import PySCFDriver
+from qiskit_nature.second_q.mapppers import ParityMapper
+from qiskit_nature.second_q.circuit import HartreeFock
 
-"""
-First we construct the initial Hydrogen molecule that are positioned 0.74 Angstrom apart, in line with the bond length of a Hydrogen molecule, and run the electronic structure calculation
-"""
+
+###First: We construct the initial Hydrogen molecule that are positioned 0.74 Angstrom apart, in line with the bond length of a Hydrogen molecule, and run the electronic structure calculation
+
 driver=PySCFDriver(atom='H 0.0 0.0 0.0; H 0.0 0.0 0.74')
 electronic_structure_calc=driver.run()
+
+###Second: We map this hydrogen molecule onto qubits (4 qubits; 2 spin states and 2 spatial orbitals)
+mapping=ParityMapper()
+
+###Third: We construct the initial state for the quantum circuit
+initial=HartreFock(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping)
+
+###Fourth: We construct the ansatz (the search circuit)
+
+
+###Fifth: Perform VQE
+
+
+###Sixth: execute and check results
 

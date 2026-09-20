9dcb9f7e6cd5475e67f6f6ed2cfc58213d891b07
Muna S <munasaber@gmail.com>
2026-09-19 23:33:23 -0700

    Constructed ansatz for hydrogen molecule

--- molecular_orbitals/Hydrogen.py
+++ molecular_orbitals/Hydrogen.py
@@ -3,6 +3,7 @@ import qiskit
 from qiskit_nature.second_q.drivers import PySCFDriver
 from qiskit_nature.second_q.mapppers import ParityMapper
 from qiskit_nature.second_q.circuit import HartreeFock
+from qiskit_nature.second_q.circuit.libary import UCCSC
 
 
 ###First: We construct the initial Hydrogen molecule that are positioned 0.74 Angstrom apart, in line with the bond length of a Hydrogen molecule, and run the electronic structure calculation
@@ -16,8 +17,8 @@ mapping=ParityMapper()
 ###Third: We construct the initial state for the quantum circuit
 initial=HartreFock(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping)
 
-###Fourth: We construct the ansatz (the search circuit)
-
+###Fourth: We construct the ansatz (the search circuit) with the UCC ansatz
+ansatz=UCCSD(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping, reps=1, initial_state=initial, generalized=False, preserve_spin=True, include_imaginary=False)
 
 ###Fifth: Perform VQE
 

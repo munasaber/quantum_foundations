40aedbb78f6fe11f9660b5e39c758800b1413c44
Muna S <munasaber@gmail.com>
2026-09-20 22:22:45 -0700

    VQE solver and execution on the Hydrogen molecule

--- molecular_orbitals/Hydrogen.py
+++ molecular_orbitals/Hydrogen.py
@@ -1,10 +1,13 @@
 import qiskit_nature
 import qiskit
 from qiskit_nature.second_q.drivers import PySCFDriver
-from qiskit_nature.second_q.mapppers import ParityMapper
-from qiskit_nature.second_q.circuit import HartreeFock
-from qiskit_nature.second_q.circuit.libary import UCCSC
-
+from qiskit_nature.second_q.mappers import ParityMapper
+from qiskit_nature.second_q.circuit.library import HartreeFock
+from qiskit_nature.second_q.circuit.library import UCCSD
+from qiskit_aer.primitives import EstimatorV2
+from qiskit_algorithms.optimizers import SPSA
+from qiskit_algorithms import VQE
+from qiskit_nature.second_q.algorithms import GroundStateEigensolver
 
 ###First: We construct the initial Hydrogen molecule that are positioned 0.74 Angstrom apart, in line with the bond length of a Hydrogen molecule, and run the electronic structure calculation
 
@@ -15,13 +18,19 @@ electronic_structure_calc=driver.run()
 mapping=ParityMapper()
 
 ###Third: We construct the initial state for the quantum circuit
-initial=HartreFock(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping)
+initial=HartreeFock(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping)
 
 ###Fourth: We construct the ansatz (the search circuit) with the UCC ansatz
 ansatz=UCCSD(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping, reps=1, initial_state=initial, generalized=False, preserve_spin=True, include_imaginary=False)
 
 ###Fifth: Perform VQE
+estimator=EstimatorV2(options={"default_precision": 1e-2})
+optimizer=SPSA(maxiter=50)
+vqe=VQE(estimator, ansatz, optimizer)
+algorithm_orchestration=GroundStateEigensolver(mapping, vqe)
+
+###Sixth: Execute
+result=algorithm_orchestration.solve(electronic_structure_calc)
 
 
-###Sixth: execute and check results
-
+###Seventh: Check results

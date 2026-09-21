b571fe595d8b79f903863016644066852185d9fe
Muna S <munasaber@gmail.com>
2026-09-20 22:24:15 -0700

    Added documentation on the VQE stage

--- molecular_orbitals/Hydrogen.py
+++ molecular_orbitals/Hydrogen.py
@@ -10,7 +10,6 @@ from qiskit_algorithms import VQE
 from qiskit_nature.second_q.algorithms import GroundStateEigensolver
 
 ###First: We construct the initial Hydrogen molecule that are positioned 0.74 Angstrom apart, in line with the bond length of a Hydrogen molecule, and run the electronic structure calculation
-
 driver=PySCFDriver(atom='H 0.0 0.0 0.0; H 0.0 0.0 0.74')
 electronic_structure_calc=driver.run()
 
@@ -23,13 +22,13 @@ initial=HartreeFock(electronic_structure_calc.num_spatial_orbitals, electronic_s
 ###Fourth: We construct the ansatz (the search circuit) with the UCC ansatz
 ansatz=UCCSD(electronic_structure_calc.num_spatial_orbitals, electronic_structure_calc.num_particles, mapping, reps=1, initial_state=initial, generalized=False, preserve_spin=True, include_imaginary=False)
 
-###Fifth: Perform VQE
+###Fifth: Perform VQE and transforms the fermions to qubits
 estimator=EstimatorV2(options={"default_precision": 1e-2})
 optimizer=SPSA(maxiter=50)
 vqe=VQE(estimator, ansatz, optimizer)
 algorithm_orchestration=GroundStateEigensolver(mapping, vqe)
 
-###Sixth: Execute
+###Sixth: Execute 
 result=algorithm_orchestration.solve(electronic_structure_calc)
 
 
